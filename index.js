d6b59a0d4c7fe417dfe6a7302cfe8184c99858ee
h-des <patrykholysz@gmail.com>
2020-08-12 18:33:18 +0200

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+import { render } from 'preact';
+
+
+render(<Foo />, document.getElementById('container'));
---
+++ src/components/app.js
@@ -0,0 +1,8 @@
+import { h, Component } from 'preact'
+
+const App = () => {
+    return <div>test</div>
+}
+
+export default App
+
